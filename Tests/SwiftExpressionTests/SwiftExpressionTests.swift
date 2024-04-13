26d8fb3a3bf89546efc9752b72f2f71b05c4bfa7
Nick <46145522+npegnatz@users.noreply.github.com>
2024-04-12 23:55:15 -0400

    Additional test cases for Expressions

--- Tests/SwiftExpressionTests/SwiftExpressionTests.swift
+++ Tests/SwiftExpressionTests/SwiftExpressionTests.swift
@@ -3,29 +3,39 @@ import XCTest
 
 final class SwiftExpressionTests: XCTestCase {
   
+  let targetAccuracy = 0.00000001
+  
   func testSimpleExpression() {
-    let expression = Expression("10 + 2")
-    XCTAssertEqual(expression.result(), 12)
+    XCTAssertEqual(Expression("10 + 2").result(), 12)
+    XCTAssertEqual(Expression("2(3)").result(), 6)
+    XCTAssertEqual(Expression("2π").result(), 2*Double.pi)
   }
   
-  func testInvalidExpression() {
-    let expression = Expression("22/")
-    XCTAssertEqual(expression.result(), nil)
+  func testComplexExpression() {
+    XCTAssertEqual(Expression("7x(2+3)", variables: ["x":3]).result(), 105)
+    XCTAssertEqual(Expression("2*x+1", variables: ["x":5]).result(), 11)
   }
   
-  func testVariableExpression() {
-    let expression = Expression("2*x+1", variables: ["x":5])
-    XCTAssertEqual(expression.result(), 11)
-  }
-  
-  func testAbsoluteValue() {
-    let expression = Expression("abs(x)", variables: ["x":-7])
-    XCTAssertEqual(expression.result(), 7)
+  func testMathFunctionExpression() {
+    XCTAssertEqual(Expression("abs(-10)").result(), 10)
+    XCTAssertEqual(Expression("abs(x)", variables: ["x":-7]).result(), 7)
+    XCTAssertEqual(Expression("sqrt(4)").result(), 2)
+    XCTAssertEqual(Expression("exp(1)").result()!, 2.7182818284, accuracy: targetAccuracy)
   }
   
   func testTrigExpression() {
-    let expression = Expression("cos(0)")
-    XCTAssertEqual(expression.result(), 1)
+    XCTAssertEqual(Expression("cos(0)").result(), 1)
+    XCTAssertEqual(Expression("sin(π/2)").result(), 1)
+    XCTAssertEqual(Expression("2*sin(cos(π))").result()!, -1.68294196962, accuracy: targetAccuracy)
+  }
+  
+  func testValidity() {
+    XCTAssertTrue(Expression("abs(x)").isValid())
+    XCTAssertTrue(Expression("(32.452*43.234)/(sqrt(10))").isValid())
+    
+    XCTAssertFalse(Expression("The quick brown fox jumped over the lazy dog").isValid())
+    XCTAssertFalse(Expression("🤓").isValid())
+    XCTAssertFalse(Expression("22/").isValid())
   }
   
   func testEqualExpressions() {
